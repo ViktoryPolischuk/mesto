8ccb69949c75996dec00e1a94dc8d03d25775a0c
Viktoria Polischuk <mrs.polischuk@yandex.ru>
2023-01-10 03:20:29 +0300

    feat: добавить редактирование профиля

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+let editButton = document.querySelector('.profile__edit-button');
+let nameElement = document.querySelector('.profile__author');
+let jobElement = document.querySelector('.profile__description');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let formElement = popup.querySelector('.popup__container');
+let nameInput = popup.querySelector('.popup__input_name');
+let jobInput = popup.querySelector('.popup__input_job');
+
+function handleEditClick () {
+  let name = nameElement.textContent;
+  let job = jobElement.textContent;
+  nameInput.value = name;
+  jobInput.value = job;
+  popup.classList.add('popup_opened');
+}
+
+function handleFormClose () {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    let name = nameInput.value;
+    let job = jobInput.value;
+    nameElement.textContent = name;
+    jobElement.textContent = job;
+    handleFormClose();
+}
+
+editButton.addEventListener('click', handleEditClick);
+closeButton.addEventListener('click', handleFormClose);
+formElement.addEventListener('submit', handleFormSubmit);
